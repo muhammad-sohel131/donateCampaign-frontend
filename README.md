cd6f026e86cbb3cfdad9db4f126365cdc18b6002
sohel <sohelf131@gmail.com>
2024-12-07 16:50:33 +0600

    added readme.md file

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-# React + Vite
+Crowdcube is a platform where users can create, browse, and support fundraising campaigns for various causes. It offers a user-friendly interface and features designed to enhance the fundraising experience.
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## 🔗 Live Site URL
+[Visit Crowdcube Live](https://donation-campaign-a1e70.web.app)
 
-Currently, two official plugins are available:
+## 🚀 Features
+- **Browse Campaigns**: View all campaigns with detailed descriptions, minimum donation amounts, and deadlines.
+- **Create Campaigns**: Authenticated users can create and manage their own campaigns.
+- **Donate to Campaigns**: Securely support campaigns directly through the platform.
+- **Sort Campaigns**: Sort campaigns by the minimum donation amount for easier navigation.
+- **User Authentication**: Login and registration functionality to provide a personalized experience.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## 🛠️ Technologies Used
+- **React.js**: Front-end framework for building a dynamic user interface.
+- **Tailwind CSS**: Styling for responsive and modern design.
+- **React-awesome-reveal**: Animated graphics to home page for enhancing the user experience.
+- **React Tooltip**: Tooltip functionality for showing username when hover on image.
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react"
 import auth from "../firebase/firbase.config";
-import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { onAuthStateChanged, signOut} from "firebase/auth";
 
 export const AuthContext = createContext();
 
@@ -9,37 +9,19 @@ export default function AuthProvider({children}) {
     const [loading, setLoading] = useState(true);
     const [isDarkMode, setIsDarkMode] = useState(false);
 
-    // const googleProvider = new GoogleAuthProvider();
-    // const signInWithGoogle = () => {
-    //     return signInWithPopup(auth, googleProvider);
-    // }
-    // const createNewUser = (email, password) => {
-    //     return createUserWithEmailAndPassword(auth, email, password)
-    // }
-    // const updateUserProfile = (updateData) => {
-    //     return updateProfile(auth.currentUser, updateData);
-    // }
+    
+   
     const logOut = () => {
         return signOut(auth)
     }
-    // const logIn = (email, password) => {
-    //     return signInWithEmailAndPassword(auth, email, password);
-    // }
-    // const resetPassword = (email) => {
-    //     return sendPasswordResetEmail(auth, email);
-    // }
+   
     const authInfo = {
         user,
         setUser,
         isDarkMode,
         setIsDarkMode,
-        // createNewUser,
         logOut,
-        // logIn,
         loading,
-        // updateUserProfile,
-        // resetPassword,
-        // signInWithGoogle
     }
 
     useEffect(() => {
